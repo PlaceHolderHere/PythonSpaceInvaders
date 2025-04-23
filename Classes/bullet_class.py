9727db0fdc7106e1d0f25e72a64c930616a6bef2
PlaceHolderHere <holderhereinplace@gmail.com>
2025-04-23 23:20:24 +0800

    Added a bullet_class.py, will be used for both players and aliens

---
+++ Classes/bullet_class.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Bullet:
+    def __init__(self, x, y, vel_y, sprite):
+        self.x = x
+        self.y = y
+        self.vel_y = vel_y
+        self.sprite = sprite
+        self.size_x = sprite.get_width()
+        self.size_y = sprite.get_height()
+        self.vel_y = 0
+
+    def blit(self, win):
+        win.blit(self.sprite, (self.x, self.y))
+
+    def move(self):
+        self.y += self.vel_y
+
+    def is_colliding(self, collide_rect):
+        self_rect = pygame.rect.Rect(self.x, self.y, self.size_x, self.size_y)
+        return self_rect.colliderect(collide_rect)
