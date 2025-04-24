d041a727aeffee22032817ca1d29c68f33ba7902
PlaceHolderHere <holderhereinplace@gmail.com>
2025-04-24 22:26:16 +0800

    Added ability for player to shoot and add bullets to the bullets array

--- Classes/player_class.py
+++ Classes/player_class.py
@@ -1,5 +1,8 @@
+from Classes.bullet_class import Bullet
+
+
 class Player:
-    def __init__(self, x, y, sprite):
+    def __init__(self, x, y, sprite, bullet_sprite):
         self.x = x
         self.y = y
         self.sprite = sprite
@@ -9,6 +12,8 @@ class Player:
         self.move_right = False
         self.alive = True
         self.lives = 3
+        self.bullets = []
+        self.bullet_sprite = bullet_sprite
 
     def blit(self, win):
         win.blit(self.sprite, (self.x, self.y))
@@ -19,3 +24,7 @@ class Player:
             self.x += 5
         if self.move_left:
             self.x -= 5
+
+    def shoot(self):
+        self.bullets.append(
+            Bullet(self.x + (self.size_x // 2), self.y + self.bullet_sprite.get_height(), self.bullet_sprite, 10))
