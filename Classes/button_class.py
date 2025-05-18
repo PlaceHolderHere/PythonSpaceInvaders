c64423f3c000baadef03d1a3e8ce84aee5098901
PlaceHolderHere <holderhereinplace@gmail.com>
2025-05-18 22:00:38 +0800

    Added a Button Class for buttons in the UI

---
+++ Classes/button_class.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+class Button:
+    def __init__(self, x, y, image, transparency):
+        self.x = x
+        self.y = y
+        self.transparency = transparency
+        self.clicked = False
+        self.button_cooldown = 0
+        self.image = image
+        self.rect = pygame.rect.Rect(x, y, image.get_width(), image.get_height())
+
+    def draw(self, win):
+        self.image.set_alpha(self.transparency)
+        win.blit(self.image, self.rect)
+
+    def is_clicked(self):
+        if self.mouse_is_hovering():
+            if pygame.mouse.get_pressed()[0] == 1 and self.button_cooldown <= 0:
+                self.button_cooldown = 15
+                return True
+
+    def mouse_is_hovering(self):
+        mouse_position = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_position):
+            self.transparency = 255
+            return True
